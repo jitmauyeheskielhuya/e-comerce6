2b83651d227f776a6b3cc212fabeb221e5b3ac0e
jitmauyeheskielhuya <jitmauyeheskiel@mail.com>
2023-06-02 20:38:52 +0900

    custom view

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,6 @@ class Home extends BaseController
 {
     public function index()
     {
-    return view('admin/home_admin');
+        return view('template/home');
     }
 }
--- app/Views/template/home.php
+++ app/Views/template/home.php
@@ -1,4 +1,4 @@
-<?= $this->extend('admin/template/layout_admin'); ?>
+<?= $this->extend('template/layout/layout_home'); ?>
 
 <?= $this->Section('page-content'); ?>
 <?php if (in_groups("admin")) : ?>
@@ -14,11 +14,12 @@
 <?php if (in_groups("pengrajin")) : ?>
   <div class="page-wrapper">
     <div class="content container-fluid">
-      <h4 class="text-emerald-400 pb-60">Dashboard pengrajin</h4>
+      <h4 class="text-emerald-400 pb-60">Dashboard Pengrajin</h4>
     </div>
     <footer class="bg-slate-400 mt-96 rounded-t-md">
       <p class="text-emerald-600">Copyright © <?= date('Y'); ?> Yeheskiel Jitmau</p>
     </footer>
   </div>
 <?php endif; ?>
+
 <?= $this->endSection(); ?>
