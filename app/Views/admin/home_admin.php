acffdb31baa3f25bc93277cf88a72f787e4b81e2
jitmauyeheskielhuya <jitmauyeheskiel@mail.com>
2023-06-02 12:06:41 +0900

    modifikasi ci4nokn

--- app/Views/admin/home_admin.php
+++ app/Views/admin/home_admin.php
@@ -1,12 +1,24 @@
 <?= $this->extend('admin/template/layout_admin'); ?>
 
 <?= $this->Section('page-content'); ?>
-<div class="page-wrapper">
-  <div class="content container-fluid">
-    <h4 class="text-emerald-400 pb-60">Dashboard Admin</h4>
+<?php if (in_groups("admin")) : ?>
+  <div class="page-wrapper">
+    <div class="content container-fluid">
+      <h4 class="text-emerald-400 pb-60">Dashboard Admin</h4>
+    </div>
+    <footer class="bg-slate-400 mt-96 rounded-t-md">
+      <p class="text-emerald-600">Copyright © <?= date('Y'); ?> Yeheskiel Jitmau</p>
+    </footer>
   </div>
-  <footer class="bg-slate-400 mt-96 rounded-t-md">
-    <p class="text-emerald-600">Copyright © <?= date('Y'); ?> Yeheskiel Jitmau</p>
-  </footer>
-</div>
+<?php endif; ?>
+<?php if (in_groups("pengrajin")) : ?>
+  <div class="page-wrapper">
+    <div class="content container-fluid">
+      <h4 class="text-emerald-400 pb-60">Dashboard pengrajin</h4>
+    </div>
+    <footer class="bg-slate-400 mt-96 rounded-t-md">
+      <p class="text-emerald-600">Copyright © <?= date('Y'); ?> Yeheskiel Jitmau</p>
+    </footer>
+  </div>
+<?php endif; ?>
 <?= $this->endSection(); ?>
